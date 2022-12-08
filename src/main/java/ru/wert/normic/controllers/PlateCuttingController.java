f51bbe5cffe4253b134eaed2c619aa2789851af0
Aleksei V. Simonov <wert001@yandex.ru>
2022-12-08 17:13:34 +0300

    save till tomorrow

--- src/main/java/ru/wert/normic/controllers/PlateCuttingController.java
+++ src/main/java/ru/wert/normic/controllers/PlateCuttingController.java
@@ -70,7 +70,7 @@ public class PlateCuttingController extends AbstractOpPlate {
         this.controller = controller;
         this.partController = (FormDetailController) controller;
         this.opData = opData;
-//        controller.getAddedPlates().add(this);
+        controller.getAddedPlates().add(this);55555555!!!!!
         controller.getAddedOperations().add(opData);
 
         new TFColoredInteger(tfHoles, this);

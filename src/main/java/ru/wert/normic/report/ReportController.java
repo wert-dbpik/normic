e9a4bd9d65d11a1f497e6f1c12a80a0d13a6fe2e
Aleksei V. Simonov <wert001@yandex.ru>
2024-05-20 07:59:08 +0300

    add ReportPacking

--- src/main/java/ru/wert/normic/report/ReportController.java
+++ src/main/java/ru/wert/normic/report/ReportController.java
@@ -14,10 +14,7 @@ import ru.wert.normic.entities.ops.opPaint.OpPaint;
 import ru.wert.normic.entities.ops.opPaint.OpPaintAssm;
 import ru.wert.normic.enums.EColor;
 import ru.wert.normic.interfaces.IOpWithOperations;
-import ru.wert.normic.report.reports.ReportMaterials;
-import ru.wert.normic.report.reports.ReportNormsByJobTypes;
-import ru.wert.normic.report.reports.ReportNormsByNormTypes;
-import ru.wert.normic.report.reports.ReportPainting;
+import ru.wert.normic.report.reports.*;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -46,15 +43,7 @@ public class ReportController {
     private double componentA; //Компонент полиэфирный А
     private double componentB; //Компонент изоцинат Б
 
-    //Расход на упаковку
-    private double cartoon;
-    private double cartoonAngle;
-    private double stretchMachine;
-    private double stretchHand;
-    private double polyTape;
-    private double bubble;
-    private double duct;
-    private double pallet;
+
 
     public ReportController() {
         steelDensity = DENSITIES.findByName("сталь");
@@ -91,11 +80,7 @@ public class ReportController {
         new ReportPainting(textReport, opAssm).create();
 
         //УПАКОВКА
-        collectPack(ops);
-
-        if(cartoon + cartoonAngle + stretchMachine + stretchHand + polyTape + bubble + duct + pallet != 0.0)
-            addPackReport(cartoon, cartoonAngle, stretchMachine, stretchHand, polyTape, bubble, duct, pallet);
-
+        new ReportPacking(textReport, opAssm).create();
 
 
         //НОРМЫ ВРЕМЕНИ ПО ВИДАМ РАБОТ
@@ -136,123 +121,12 @@ public class ReportController {
         }
     }
 
-//==========   ПОКРАСКА   ===================================================
-
-    /**
-     * Добавить отчет по РАСХОДУ КРАСКИ (начало)
-     */
-    private void addColorReport(List<Double> ral1, List<Double> ral2, List<Double> ral3) {
-        textReport.append("\n\n").append("ПОКРЫТИЕ :\n");
-        if(ral1.get(0) != 0.0) addRal1Report(ral1, EColor.COLOR_I);
-        if(ral2.get(0) != 0.0) addRal1Report(ral2, EColor.COLOR_II);
-        if(ral3.get(0) != 0.0) addRal1Report(ral3, EColor.COLOR_III);
-    }
-    /**
-     * Добавить отчет по РАСХОДУ КРАСКИ (по конкретной краске)
-     */
-    private void addRal1Report(List<Double> ral1, EColor color) {
-        textReport.append("Краска '")
-                .append(color.getRal())
-                .append("', площадь = ")
-                .append(DECIMAL_FORMAT.format(ral1.get(0)))
-                .append(" м.кв., ")
-                .append("расход = ")
-                .append(DECIMAL_FORMAT.format(ral1.get(1)))
-                .append(" кг.\n");
-    }
-
-    /**
-     * Сосчитать СУММАРНУЮ ПЛОЩАДЬ и РАСХОД краски
-     */
-    private List<Double> collectListOfOperationsInOpData(IOpWithOperations op, EColor color){
-        double area = 0.0;
-        double weight = 0.0;
-        for(OpData o : op.getOperations()){
-            if(o instanceof IOpWithOperations) {
-                List<Double> ress = collectListOfOperationsInOpData((IOpWithOperations) o, color);
-                area += ress.get(0);
-                weight += ress.get(1);
-            }else{
-                if(o instanceof OpPaint && ((OpPaint)o).getColor().equals(color)) {
-                    area += ((OpPaint) o).getArea() * op.getOpData().getQuantity();
-                    weight += ((OpPaint) o).getDyeWeight() * op.getOpData().getQuantity();
-                }else if(o instanceof OpPaintAssm && ((OpPaintAssm)o).getColor().equals(color)) {
-                    area += ((OpPaintAssm) o).getCountedArea() * op.getOpData().getQuantity();
-                    weight += ((OpPaintAssm) o).getDyeWeight() * op.getOpData().getQuantity();
-                }
-            }
-
-        }
-        return Arrays.asList(area, weight);
-    }
 
 
 //==========   УПАКОВКА   ===================================================
-    /**
-     * Сосчитать всю упаковку
-     */
-    private void collectPack(List<OpData> ops) {
-        for (OpData op : ops) {
-            if (op instanceof OpAssm || op instanceof OpPack) {
-                List<OpData> opsInAssm = ((IOpWithOperations) op).getOperations();
-                collectPack(opsInAssm);
-            } else if (op instanceof PackingData) {
-                cartoon += ((PackingData) op).getCartoon();
-                cartoonAngle += ((PackingData) op).getCartoonAngle();
-                stretchMachine += ((PackingData) op).getStretchMachineWrap();
-                stretchHand += ((PackingData) op).getStretchHandWrap();
-                polyTape += ((PackingData) op).getPolyWrap();
-                bubble += ((PackingData) op).getBubbleWrap();
-                duct += ((PackingData) op).getDuctTape();
-                pallet += ((PackingData) op).getPallet();
-            }
-        }
-    }
 
-    /**
-     * Добавить отчет по УПАКОВКЕ
-     */
-    private void addPackReport(double cartoon, double cartoonAngle, double stretchMachine, double stretchHand, double polyTape,
-                               double bubble, double duct, double pallet){
-        textReport.append("\n\n").append("УПАКОВКА :\n");
-        if (cartoon != 0.0)
-            textReport.append(CARTOON.getName())
-                    .append(DECIMAL_FORMAT.format(cartoon)).append(" ")
-                    .append(CARTOON.getMeasuring()).append("\n");
-        if (cartoonAngle != 0.0)
-            textReport.append(CARTOON_ANGLE.getName())
-                    .append(DECIMAL_FORMAT.format(cartoonAngle)).append(" ")
-                    .append(CARTOON_ANGLE.getMeasuring()).append("\n");
-        if (stretchMachine != 0.0)
-            textReport.append(STRETCH_MACHINE.getName())
-                    .append(DECIMAL_FORMAT.format(stretchMachine)).append(" ")
-                    .append(STRETCH_MACHINE.getMeasuring()).append("\n");
-        if (stretchHand != 0.0)
-            textReport.append(STRETCH_HAND.getName())
-                    .append(DECIMAL_FORMAT.format(stretchHand)).append(" ")
-                    .append(STRETCH_HAND.getMeasuring()).append("\n");
-        if (polyTape != 0.0)
-            textReport.append(POLY.getName())
-                    .append(DECIMAL_FORMAT.format(polyTape)).append(" ")
-                    .append(POLY.getMeasuring()).append("\n");
-        if (bubble != 0.0)
-            textReport.append(BUBBLE.getName())
-                    .append(DECIMAL_FORMAT.format(bubble)).append(" ")
-                    .append(BUBBLE.getMeasuring()).append("\n");
-        if (duct != 0.0)
-            textReport.append(DUCT.getName())
-                    .append(DECIMAL_FORMAT.format(duct)).append(" ")
-                    .append(DUCT.getMeasuring()).append("\n");
-        if (pallet != 0.0)
-            textReport.append(PALLET.getName())
-                    .append(DECIMAL_FORMAT.format(pallet)).append(" ")
-                    .append(PALLET.getMeasuring()).append("\n");
-        textReport.append("Этикетка-самоклеящаяся 100х80/500\n");
-        textReport.append("Этикетка-самоклеящаяся 58х30/1000 ПП Серебро\n");
-        textReport.append("Наклейка с логотипом ПИК, 3 цвета\n");
-    }
 
-//==========   НОРМЫ ВРЕМЕНИ   ===================================================
+
 
 
 }
---
+++ src/main/java/ru/wert/normic/report/reports/ReportPacking.java
@@ -0,0 +1,105 @@
+package ru.wert.normic.report.reports;
+
+import ru.wert.normic.entities.ops.OpData;
+import ru.wert.normic.entities.ops.opPack.PackingData;
+import ru.wert.normic.entities.ops.single.OpAssm;
+import ru.wert.normic.entities.ops.single.OpPack;
+import ru.wert.normic.interfaces.IOpWithOperations;
+
+import java.util.List;
+
+import static ru.wert.normic.controllers.AbstractOpPlate.DECIMAL_FORMAT;
+import static ru.wert.normic.enums.EPacks.*;
+
+public class ReportPacking {
+
+    //Расход на упаковку
+    private double cartoon;
+    private double cartoonAngle;
+    private double stretchMachine;
+    private double stretchHand;
+    private double polyTape;
+    private double bubble;
+    private double duct;
+    private double pallet;
+
+    private StringBuilder textReport;
+    private OpAssm opAssm;
+
+    public ReportPacking(StringBuilder textReport, OpAssm opAssm) {
+        this.textReport = textReport;
+        this.opAssm = opAssm;
+    }
+
+    public void create() {
+        collectPack(opAssm.getOperations());
+
+        if(cartoon + cartoonAngle + stretchMachine + stretchHand + polyTape + bubble + duct + pallet != 0.0)
+            addPackReport(cartoon, cartoonAngle, stretchMachine, stretchHand, polyTape, bubble, duct, pallet);
+    }
+
+    /**
+     * Сосчитать всю упаковку
+     */
+    private void collectPack(List<OpData> ops) {
+        for (OpData op : ops) {
+            if (op instanceof OpAssm || op instanceof OpPack) {
+                List<OpData> opsInAssm = ((IOpWithOperations) op).getOperations();
+                collectPack(opsInAssm);
+            } else if (op instanceof PackingData) {
+                cartoon += ((PackingData) op).getCartoon();
+                cartoonAngle += ((PackingData) op).getCartoonAngle();
+                stretchMachine += ((PackingData) op).getStretchMachineWrap();
+                stretchHand += ((PackingData) op).getStretchHandWrap();
+                polyTape += ((PackingData) op).getPolyWrap();
+                bubble += ((PackingData) op).getBubbleWrap();
+                duct += ((PackingData) op).getDuctTape();
+                pallet += ((PackingData) op).getPallet();
+            }
+        }
+    }
+
+    /**
+     * Добавить отчет по УПАКОВКЕ
+     */
+    private void addPackReport(double cartoon, double cartoonAngle, double stretchMachine, double stretchHand, double polyTape,
+                               double bubble, double duct, double pallet){
+        textReport.append("\n\n").append("УПАКОВКА :\n");
+        if (cartoon != 0.0)
+            textReport.append(CARTOON.getName())
+                    .append(DECIMAL_FORMAT.format(cartoon)).append(" ")
+                    .append(CARTOON.getMeasuring()).append("\n");
+        if (cartoonAngle != 0.0)
+            textReport.append(CARTOON_ANGLE.getName())
+                    .append(DECIMAL_FORMAT.format(cartoonAngle)).append(" ")
+                    .append(CARTOON_ANGLE.getMeasuring()).append("\n");
+        if (stretchMachine != 0.0)
+            textReport.append(STRETCH_MACHINE.getName())
+                    .append(DECIMAL_FORMAT.format(stretchMachine)).append(" ")
+                    .append(STRETCH_MACHINE.getMeasuring()).append("\n");
+        if (stretchHand != 0.0)
+            textReport.append(STRETCH_HAND.getName())
+                    .append(DECIMAL_FORMAT.format(stretchHand)).append(" ")
+                    .append(STRETCH_HAND.getMeasuring()).append("\n");
+        if (polyTape != 0.0)
+            textReport.append(POLY.getName())
+                    .append(DECIMAL_FORMAT.format(polyTape)).append(" ")
+                    .append(POLY.getMeasuring()).append("\n");
+        if (bubble != 0.0)
+            textReport.append(BUBBLE.getName())
+                    .append(DECIMAL_FORMAT.format(bubble)).append(" ")
+                    .append(BUBBLE.getMeasuring()).append("\n");
+        if (duct != 0.0)
+            textReport.append(DUCT.getName())
+                    .append(DECIMAL_FORMAT.format(duct)).append(" ")
+                    .append(DUCT.getMeasuring()).append("\n");
+        if (pallet != 0.0)
+            textReport.append(PALLET.getName())
+                    .append(DECIMAL_FORMAT.format(pallet)).append(" ")
+                    .append(PALLET.getMeasuring()).append("\n");
+        textReport.append("Этикетка-самоклеящаяся 100х80/500\n");
+        textReport.append("Этикетка-самоклеящаяся 58х30/1000 ПП Серебро\n");
+        textReport.append("Наклейка с логотипом ПИК, 3 цвета\n");
+    }
+
+}
